ab0d853455b444b6ab75fcf8c59f4c4aca530422
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-13 12:20:27 +0530

    api key

--- src/contracts/hardhat.config.js
+++ src/contracts/hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+const fs = require('fs')
 
+const privateKey = fs.readFileSync(".secret").toString()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/aSfcG6PPf_B5d08_OUsz1KOOEYTdnOI9",
+      accounts: [privateKey]
+    },
+    mainnet: {}
+  }
 };
