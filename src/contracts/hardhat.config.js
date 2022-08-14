727b964b538da4186a862843ebb4f8b6afb0f4f1
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-14 08:21:48 +0530

    pvt key

--- src/contracts/hardhat.config.js
+++ src/contracts/hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers");
 const fs = require('fs')
 
-const privateKey = fs.readFileSync(".secret").toString()
+const privateKey = fs.readFileSync(".secret").toString().trim()
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
@@ -10,6 +10,9 @@ module.exports = {
     hardhat: {
       chainId: 1337,
     },
-    
+    mumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/aSfcG6PPf_B5d08_OUsz1KOOEYTdnOI9",
+      accounts: [privateKey]
+    },
   }
 };
